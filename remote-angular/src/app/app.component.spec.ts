b8c6567f2dc5ce4c88273d01a89d736bb94242f2
Зильберман Марк Евгеньевич <mzilberman@cinimex.ru>
2025-05-16 15:25:55 +0300

    changed naming

--- remote-angular/src/app/app.component.spec.ts
+++ remote-angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'components-angular-microfront' title`, () => {
+  it(`should have the 'remote-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('components-angular-microfront');
+    expect(app.title).toEqual('remote-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, components-angular-microfront');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, remote-angular');
   });
 });
